724355f79d44873ef667d108bb7abae945a799f1
Tony.Wu <uq44961879@gmail.com>
2023-07-28 14:50:16 +1000

    <feat>: allow CORS at server

--- app.js
+++ app.js
@@ -24,6 +24,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// allow CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+})
+
+
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 
